806ea6a9ded343b3b3242df4851fac00a96d72aa
Victor <victor.alarcon@utp.edu.co>
2024-03-25 09:22:35 -0500

    se avanzo

--- Consultas/Emisor_Consulta.cs
+++ Consultas/Emisor_Consulta.cs
@@ -31,20 +31,34 @@ namespace GeneradorCufe.Consultas
                     // Abrir la conexión
                     connection.Open();
 
-                    // Define tu consulta SQL
-                    string query = "SELECT * FROM movimiento";
+                    // Define tu consulta SQL para seleccionar los campos deseados de la tabla empresas
+                    string query = $"SELECT emprnombr, emprnit, emprtipo, emprdirec, emprciuda, empregim_x FROM empresas WHERE emprobra = @empresa";
 
+                    // Crear un nuevo comando SQL con la consulta y la conexión
                     using (MySqlCommand command = new MySqlCommand(query, connection))
                     {
+                        // Agregar el parámetro 'empresa' a la consulta SQL para evitar la inyección de SQL
+                        command.Parameters.AddWithValue("@empresa", empresa);
+
+                        // Crear un adaptador de datos y un DataTable para almacenar los resultados de la consulta
                         using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
                         {
                             DataTable dataTable = new DataTable();
                             adapter.Fill(dataTable);
 
-                            // Procesa los resultados de la consulta aquí
+                            // Procesar los resultados de la consulta
                             foreach (DataRow row in dataTable.Rows)
                             {
-                                
+                                // Obtener los valores de cada columna y hacer lo que necesites con ellos
+                                string emprnombr = row["emprnombr"].ToString()??"";
+                                string emprnit = row["emprnit"].ToString() ?? "";
+                                string emprtipo = row["emprtipo"].ToString() ?? "";
+                                string emprdirec = row["emprdirec"].ToString() ?? "";
+                                string emprciuda = row["emprciuda"].ToString() ?? "";
+                                string empregim_x = row["empregim_x"].ToString() ?? "";
+
+                                // Hacer lo que necesites con los valores obtenidos
+                                Console.WriteLine($"Nombre de Empresa: {emprnombr}, NIT: {emprnit}, Tipo: {emprtipo}, Dirección: {emprdirec}, Ciudad: {emprciuda}, Régimen: {empregim_x}");
                             }
                         }
                     }
@@ -59,6 +73,7 @@ namespace GeneradorCufe.Consultas
                 Console.WriteLine("Error al ejecutar la acción para la IP: " + ex.Message);
             }
         }
+
 
 
 
