fb73fecf5038371755e5a5a40741f36a653a36cb
Victor <victor.alarcon@utp.edu.co>
2024-05-21 20:37:35 -0500

    hace falta el PartyIdentification

--- ViewModel/GenerarAdquiriente.cs
+++ ViewModel/GenerarAdquiriente.cs
@@ -31,118 +31,124 @@ namespace GeneradorCufe.ViewModel
             string AdditionalAccountID = (adquiriente.Tipo_p == 1) ? "2" : "1";
 
             // Información del adquiriente
-            var accountingCustomerPartyElement = xmlDoc.Descendants(cac + "AccountingCustomerParty").FirstOrDefault();
-            if (accountingCustomerPartyElement != null)
+            try
             {
-                accountingCustomerPartyElement.Element(cbc + "AdditionalAccountID")?.SetValue(AdditionalAccountID); // Identificador de tipo de adquiriente  jurídica de la de persona
-
-                // Información del adquiriente
-                var partyElement = accountingCustomerPartyElement.Element(cac + "Party");
-                if (partyElement != null)
+                var accountingCustomerPartyElement = xmlDoc.Descendants(cac + "AccountingCustomerParty").FirstOrDefault();
+                if (accountingCustomerPartyElement != null)
                 {
-                    var partyIdentificationElement = partyElement.Element(cac + "PartyIdentification");
-                    if (partyIdentificationElement != null)
+                    accountingCustomerPartyElement.Element(cbc + "AdditionalAccountID")?.SetValue(AdditionalAccountID); // Identificador de tipo de adquiriente  jurídica de la de persona
+
+                    // Información del adquiriente
+                    var partyElement = accountingCustomerPartyElement.Element(cac + "Party");
+                    if (partyElement != null)
                     {
-                        var idElement = partyIdentificationElement.Element(cbc + "ID");
-                        if (idElement != null)
+                        var partyIdentificationElement = partyElement.Element(cac + "PartyIdentification");
+                        if (partyIdentificationElement != null)
                         {
-                            idElement.Value = adquiriente.Nit_adqui;
-                            idElement.SetAttributeValue("schemeName", Tipo); // cambio de 31 a 13
-                        }
-
-
-                        if (partyElement != null)
-                        {
-                            partyElement.Element(cac + "PartyName")?.Element(cbc + "Name")?.SetValue(adquiriente.Nombre_adqu);
-
-                            // Información de ubicación física del adquiriente
-                            var physicalLocationElement = partyElement.Element(cac + "PhysicalLocation");
-                            if (physicalLocationElement != null)
+                            var idElement = partyIdentificationElement.Element(cbc + "ID");
+                            if (idElement != null)
                             {
-                                physicalLocationElement.Element(cac + "Address")?.Element(cbc + "ID")?.SetValue(codigos.Codigo_Municipio);
-                                physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CityName")?.SetValue(Municipio);
-                                physicalLocationElement.Element(cac + "Address")?.Element(cbc + "PostalZone")?.SetValue("660001");
-                                physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CountrySubentity")?.SetValue(Departamento);
-                                physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CountrySubentityCode")?.SetValue(codigos.Codigo_Departamento);
-                                physicalLocationElement.Element(cac + "Address")?.Element(cac + "Country")?.Element(cbc + "IdentificationCode")?.SetValue("CO");
-                                physicalLocationElement.Element(cac + "Address")?.Element(cac + "Country")?.Element(cbc + "Name")?.SetValue("Colombia");
-                                physicalLocationElement.Element(cac + "Address")?.Element(cac + "AddressLine")?.Element(cbc + "Line")?.SetValue(adquiriente.Direccion_adqui);
-                            }
-
-                            // Información tributaria del adquiriente
-                            var partyTaxSchemeElement = partyElement.Element(cac + "PartyTaxScheme");
-                            if (partyTaxSchemeElement != null)
-                            {
-                                // Establecer el nombre de registro
-                                partyTaxSchemeElement.Element(cbc + "RegistrationName")?.SetValue(adquiriente.Nombre_adqu);
-
-                                // Establecer el ID de la compañía
-                                var companyIDElement = partyTaxSchemeElement.Element(cbc + "CompanyID");
-                                if (companyIDElement != null)
-                                {
-                                    companyIDElement.SetValue(adquiriente.Nit_adqui);
-                                    companyIDElement.SetAttributeValue("schemeID", adquiriente.Dv_Adqui);
-                                    companyIDElement.SetAttributeValue("schemeName", Tipo);
-                                    companyIDElement.SetAttributeValue("schemeAgencyID", "195");
-                                    companyIDElement.SetAttributeValue("schemeAgencyName", "CO, DIAN (Dirección de Impuestos y Aduanas Nacionales)");
-                                }
-
-                                // Establecer el código de nivel de impuestos
-                                partyTaxSchemeElement.Element(cbc + "TaxLevelCode")?.SetValue("R-99-PN");
-
-                                // Información de ubicación de registro tributario del adquiriente
-                                var registrationAddressElement = partyTaxSchemeElement.Element(cac + "RegistrationAddress");
-                                if (registrationAddressElement != null)
-                                {
-                                    registrationAddressElement.Element(cbc + "ID")?.SetValue(codigos.Codigo_Municipio);
-                                    registrationAddressElement.Element(cbc + "CityName")?.SetValue(Municipio);
-                                    registrationAddressElement.Element(cbc + "PostalZone")?.SetValue("660001");
-                                    registrationAddressElement.Element(cbc + "CountrySubentity")?.SetValue(Departamento);
-                                    registrationAddressElement.Element(cbc + "CountrySubentityCode")?.SetValue(codigos.Codigo_Departamento);
-                                    registrationAddressElement.Element(cac + "Country")?.Element(cbc + "IdentificationCode")?.SetValue("CO");
-                                    registrationAddressElement.Element(cac + "Country")?.Element(cbc + "Name")?.SetValue("Colombia");
-                                    registrationAddressElement.Element(cac + "AddressLine")?.Element(cbc + "Line")?.SetValue(adquiriente.Direccion_adqui);
-                                }
-
-                                // Información del esquema tributario del adquiriente
-                                var taxSchemeElement = partyTaxSchemeElement.Element(cac + "TaxScheme");
-                                if (taxSchemeElement != null)
-                                {
-                                    taxSchemeElement.Element(cbc + "ID")?.SetValue("01");
-                                    taxSchemeElement.Element(cbc + "Name")?.SetValue("IVA");
-                                }
-                            }
-
-                            // Información legal del adquiriente
-                            var partyLegalEntityElement = partyElement.Element(cac + "PartyLegalEntity");
-                            if (partyLegalEntityElement != null)
-                            {
-                                // Establecer el nombre de registro
-                                partyLegalEntityElement.Element(cbc + "RegistrationName")?.SetValue(adquiriente.Nombre_adqu);
-
-                                // Establecer el ID de la compañía
-                                var companyIDElement = partyLegalEntityElement.Element(cbc + "CompanyID");
-                                if (companyIDElement != null)
-                                {
-                                    companyIDElement.SetValue(adquiriente.Nit_adqui);
-                                    companyIDElement.SetAttributeValue("schemeID", adquiriente.Dv_Adqui);
-                                    companyIDElement.SetAttributeValue("schemeName", Tipo);
-                                    companyIDElement.SetAttributeValue("schemeAgencyID", "195");
-                                    companyIDElement.SetAttributeValue("schemeAgencyName", "CO, DIAN (Dirección de Impuestos y Aduanas Nacionales)");
-                                }
+                                idElement.Value = adquiriente.Nit_adqui;
+                                idElement.SetAttributeValue("schemeName", Tipo); // cambio de 31 a 13
                             }
 
 
-                            // Información de contacto del adquiriente
-                            var contactElement = partyElement.Element(cac + "Contact");
-                            if (contactElement != null)
+                            if (partyElement != null)
                             {
-                                contactElement.Element(cbc + "Telephone")?.SetValue(adquiriente.Telefono_adqui);
-                                contactElement.Element(cbc + "ElectronicMail")?.SetValue(adquiriente.Correo_adqui);
+                                partyElement.Element(cac + "PartyName")?.Element(cbc + "Name")?.SetValue(adquiriente.Nombre_adqu);
+
+                                // Información de ubicación física del adquiriente
+                                var physicalLocationElement = partyElement.Element(cac + "PhysicalLocation");
+                                if (physicalLocationElement != null)
+                                {
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cbc + "ID")?.SetValue(codigos.Codigo_Municipio);
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CityName")?.SetValue(Municipio);
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cbc + "PostalZone")?.SetValue("660001");
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CountrySubentity")?.SetValue(Departamento);
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cbc + "CountrySubentityCode")?.SetValue(codigos.Codigo_Departamento);
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cac + "Country")?.Element(cbc + "IdentificationCode")?.SetValue("CO");
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cac + "Country")?.Element(cbc + "Name")?.SetValue("Colombia");
+                                    physicalLocationElement.Element(cac + "Address")?.Element(cac + "AddressLine")?.Element(cbc + "Line")?.SetValue(adquiriente.Direccion_adqui);
+                                }
+
+                                // Información tributaria del adquiriente
+                                var partyTaxSchemeElement = partyElement.Element(cac + "PartyTaxScheme");
+                                if (partyTaxSchemeElement != null)
+                                {
+                                    // Establecer el nombre de registro
+                                    partyTaxSchemeElement.Element(cbc + "RegistrationName")?.SetValue(adquiriente.Nombre_adqu);
+
+                                    // Establecer el ID de la compañía
+                                    var companyIDElement = partyTaxSchemeElement.Element(cbc + "CompanyID");
+                                    if (companyIDElement != null)
+                                    {
+                                        companyIDElement.SetValue(adquiriente.Nit_adqui);
+                                        companyIDElement.SetAttributeValue("schemeID", adquiriente.Dv_Adqui);
+                                        companyIDElement.SetAttributeValue("schemeName", Tipo);
+                                        companyIDElement.SetAttributeValue("schemeAgencyID", "195");
+                                        companyIDElement.SetAttributeValue("schemeAgencyName", "CO, DIAN (Dirección de Impuestos y Aduanas Nacionales)");
+                                    }
+
+                                    // Establecer el código de nivel de impuestos
+                                    partyTaxSchemeElement.Element(cbc + "TaxLevelCode")?.SetValue("R-99-PN");
+
+                                    // Información de ubicación de registro tributario del adquiriente
+                                    var registrationAddressElement = partyTaxSchemeElement.Element(cac + "RegistrationAddress");
+                                    if (registrationAddressElement != null)
+                                    {
+                                        registrationAddressElement.Element(cbc + "ID")?.SetValue(codigos.Codigo_Municipio);
+                                        registrationAddressElement.Element(cbc + "CityName")?.SetValue(Municipio);
+                                        registrationAddressElement.Element(cbc + "PostalZone")?.SetValue("660001");
+                                        registrationAddressElement.Element(cbc + "CountrySubentity")?.SetValue(Departamento);
+                                        registrationAddressElement.Element(cbc + "CountrySubentityCode")?.SetValue(codigos.Codigo_Departamento);
+                                        registrationAddressElement.Element(cac + "Country")?.Element(cbc + "IdentificationCode")?.SetValue("CO");
+                                        registrationAddressElement.Element(cac + "Country")?.Element(cbc + "Name")?.SetValue("Colombia");
+                                        registrationAddressElement.Element(cac + "AddressLine")?.Element(cbc + "Line")?.SetValue(adquiriente.Direccion_adqui);
+                                    }
+
+                                    // Información del esquema tributario del adquiriente
+                                    var taxSchemeElement = partyTaxSchemeElement.Element(cac + "TaxScheme");
+                                    if (taxSchemeElement != null)
+                                    {
+                                        taxSchemeElement.Element(cbc + "ID")?.SetValue("01");
+                                        taxSchemeElement.Element(cbc + "Name")?.SetValue("IVA");
+                                    }
+                                }
+
+                                // Información legal del adquiriente
+                                var partyLegalEntityElement = partyElement.Element(cac + "PartyLegalEntity");
+                                if (partyLegalEntityElement != null)
+                                {
+                                    // Establecer el nombre de registro
+                                    partyLegalEntityElement.Element(cbc + "RegistrationName")?.SetValue(adquiriente.Nombre_adqu);
+
+                                    // Establecer el ID de la compañía
+                                    var companyIDElement = partyLegalEntityElement.Element(cbc + "CompanyID");
+                                    if (companyIDElement != null)
+                                    {
+                                        companyIDElement.SetValue(adquiriente.Nit_adqui);
+                                        companyIDElement.SetAttributeValue("schemeID", adquiriente.Dv_Adqui);
+                                        companyIDElement.SetAttributeValue("schemeName", Tipo);
+                                        companyIDElement.SetAttributeValue("schemeAgencyID", "195");
+                                        companyIDElement.SetAttributeValue("schemeAgencyName", "CO, DIAN (Dirección de Impuestos y Aduanas Nacionales)");
+                                    }
+                                }
+
+
+                                // Información de contacto del adquiriente
+                                var contactElement = partyElement.Element(cac + "Contact");
+                                if (contactElement != null)
+                                {
+                                    contactElement.Element(cbc + "Telephone")?.SetValue(adquiriente.Telefono_adqui);
+                                    contactElement.Element(cbc + "ElectronicMail")?.SetValue(adquiriente.Correo_adqui);
+                                }
                             }
                         }
                     }
                 }
+            }
+            catch (Exception){
+                Console.WriteLine();
             }
         }
     }
