db8a5bfed240eecc74095163d9870e54d1f3a929
Victor <alarcongt4@gmail.com>
2024-06-12 10:59:53 -0500

    mas facturas

--- ViewModel/EnviarCorreo.cs
+++ ViewModel/EnviarCorreo.cs
@@ -28,8 +28,8 @@ namespace GeneradorCufe.ViewModel
             // Configurar el cliente SMTP
             SmtpClient clienteSmtp = new SmtpClient("mail.rmsoft.com.co");
             clienteSmtp.Port = 587;
-         //   clienteSmtp.Credentials = new NetworkCredential("facturaelectronica@rmsoft.com.co", "p6;li17^wU02");
-            clienteSmtp.Credentials = new NetworkCredential("facturaselectronicas@rmsoft.com.co", "[]Q^nFwROD[6");
+            clienteSmtp.Credentials = new NetworkCredential("facturaelectronica@rmsoft.com.co", "p6;li17^wU02");
+            //clienteSmtp.Credentials = new NetworkCredential("facturaselectronicas@rmsoft.com.co", "[]Q^nFwROD[6");
             clienteSmtp.EnableSsl = true; // Habilitar SSL
            
 
@@ -79,9 +79,9 @@ namespace GeneradorCufe.ViewModel
 
 
             mensaje.IsBodyHtml = true; // Establecer el cuerpo del mensaje como HTML
-            string rutaImagen = @"C:\Users\hp\source\repos\GeneradorCufe\xml\logo.png"; // Victor
-            /* string rutaImagen = @"C:\Users\Programacion01\source\repos\RepoVictor\GeneradorCufe\xml\logo.png";*/ // Oficina
-                                                                                                                    // string rutaImagen = @"C:\inetpub\xml\Imagenes\logo.png"; // Gigas
+            /*string rutaImagen = @"C:\Users\hp\source\repos\GeneradorCufe\xml\logo.png";*/ // Victor
+            string rutaImagen = @"C:\Users\Programacion01\source\repos\RepoVictor\GeneradorCufe\xml\logo.png"; // Oficina
+                                                                                                               // string rutaImagen = @"C:\inetpub\xml\Imagenes\logo.png"; // Gigas
             if (File.Exists(rutaImagen))
             {
                 // Agregar la imagen como un archivo adjunto al mensaje
